15cfc44a95879986f6d0af626807829488baa28f
Kalani <kalani_man@reyrey.com>
2023-05-18 17:00:58 -0500

    Add signup call

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: 'https://9601-206-180-44-25.ngrok-free.app'
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,47 @@
 import createDataContext from "./createDataContext";
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case 'add_error': 
+            return {
+                ...state, errorMessage: action.payload
+            }
         default: 
             return state;
     }
 };
 
+const signup = (dispatch) => {
+    return async ({ email, password }) => {
+        // make api request to sign up with that email and password
+        try {
+            const response = await trackerApi.post('/signup', { email, password });
+            console.log(response.data);
+        } catch (err) {
+            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up.'})
+        }
+    };
+};
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        // Try to sign in
+
+        // Handle success by updating state
+
+        // Handle failure by showing error message
+    }
+}
+
+const signout = (dispatch) => {
+    return () => {
+        // Sign out
+    }
+}
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {}, 
-    { isSignedIn: false }
+    {signin, signout, signup}, 
+    { isSignedIn: false, errorMessage: '' }
 )
