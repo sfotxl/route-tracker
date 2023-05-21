202d75bae632a51492d1ab8a50a97b6d18200f4d
Kalani Man <kromelo@icloud.com>
2023-05-21 14:41:52 -0500

    Add change name function

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,51 +1,29 @@
 import '../_mockLocation';
-import React, { useEffect, useState, useContext } from "react";
-import { StyleSheet } from "react-native";
-import { Text } from "react-native-elements";
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
 import Map from '../components/Map';
-import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from "expo-location";
-import { Context as LocationContext } from "../context/LocationContext";
+import { useLocation } from '../hooks/useLocation';
+import { Context as LocationContext } from '../context/LocationContext';
+import { withNavigationFocus } from 'react-navigation';
+import TrackForm from '../components/TrackForm';
 
-const TrackCreateScreen = () => {
-    const { addLocation } = useContext(LocationContext);
-    const [err, setErr] = useState(null);
+const TrackCreateScreen = ({ isFocused }) => {
+  const { addLocation } = useContext(LocationContext);
 
-    const startWatching = async () => {
-        try {
-          const { granted } = await requestForegroundPermissionsAsync();
-          await watchPositionAsync(
-            {
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000,
-                distanceInterval: 10
-            },
-            location => {
-                addLocation(location);
-            }
-          )
-          if (!granted) {
-            throw new Error('Location permission not granted');
-          }
-        } catch (e) {
-          setErr(e);
-        }
-      };
+  const [err] = useLocation(isFocused, addLocation);
 
-    useEffect(() => {
-        startWatching();
-      }, [])
+  return (
+    <>
+      <Text h3>Create a Track</Text>
+      <Map />
 
-    return(
-        <>
-            <Text h3>Create a Track</Text>
-            <Map />
-            {err ? <Text>Please enable loation services.</Text> : null}
-        </>
-    )
+      {err ? <Text>Please enable loation services.</Text> : null}
+      <TrackForm />
+    </>
+  );
 };
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({});
 
-});
-
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
